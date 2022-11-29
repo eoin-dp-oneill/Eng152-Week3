e60ab85ee324c6b26b1aab1ad8134dfb093d3fdb
Eoin O'Neill <eoinoneill@Eoins-MacBook-Pro.local>
2022-11-29 09:56:53 +0000

    week 3 assignments

---
+++ Labs/Collections_Lab_Starter/Collections_Lab/Collections_Lib/CollectionsExercises.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Lib
+{
+    public class CollectionsExercises
+    {
+
+        /* removes and returns the next num entries in the queue, as a comma separated string */
+        // FIFO -- first in first out
+        public static string NextInQueue(int num, Queue<string> queue)
+        {
+            Queue<string> testQueue = new Queue<string>();
+            string nextEntries = "";
+            for(int i=0;i < num;i++)
+            {
+                nextEntries = testQueue.Dequeue() + ", ";
+            }
+            return nextEntries;
+        }
+
+        /* uses a Stack to create and return array of ints in reverse order to the one supplied */
+        // LIFO - Last in first out
+        public static int[] Reverse(int[] original)
+        {
+            Stack<int> reverseStack = new Stack<int>();
+            foreach(int num in original)
+            {
+                reverseStack.Push(num);
+            }
+            return reverseStack.ToArray();
+        }
+
+        // using a Dictionary, counts and returns (as a string) the occurence of the digits 0-9 in the given string
+        public static string CountDigits(string input)
+        {
+            // key-number corresponds to count-elementt
+            string countString = "";
+            Dictionary<string, int> countDictionary = new Dictionary<string, int>();
+
+            for(int i =0;i<10;i++)
+            {
+                countDictionary.Add(i.ToString(), 0);
+            }
+
+            foreach(KeyValuePair<string,int> countVal in countDictionary)
+            {
+                foreach (string letter in input)
+                {
+                    if (letter == countVal.Key) countDictionary[countVal.Key]+=1;
+                }
+                countString += $"[{countVal.Key},{countVal.Value}]";
+            }
+
+            return countString; 
+        }
+    }
+}
---
+++ Labs/Collections_Lab_Starter/Collections_Lab/Collections_Lib/ListExercises.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Lib
+{
+    public class ListExercises
+    {
+        // returns a list of all the integers between 1 to max inclusive
+        // that are multiples of 5
+        public static List<int> MakeFiveList(int max)
+        {
+            List<int> multiplesOfFive = new List<int>();
+            for(int i=5;i<=max;i+=5)
+            {
+                multiplesOfFive.Add(i);
+            }
+            return multiplesOfFive;
+        }
+
+        // returns a list of all the strings in sourceList that start with the letter 'A' or 'a'
+        public static List<string> MakeAList(List<string> sourceList)
+        {
+            List<string> stringList = new List<string>();
+            foreach(string source in sourceList)
+            {
+                if (source[0] == 'A' || source[0] == 'a') stringList.Add(source);
+            }
+            return stringList;
+        }
+    }
+}
